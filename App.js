7a0ad5b9570d2abc4e626f64df6b7511f519ef77
yuvasreeramesh <yuvashree.doodleblue@gmail.com>
2023-07-27 13:05:42 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { View, Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './screens/HomeScreen';
+const Stack = createNativeStackNavigator();
+
+function App() {
+ 
+ 
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} /> */}
+        <Stack.Screen name="Home" component={HomeScreen}  />
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ android/app/build/generated/rncli/src/main/java/com/facebook/react/PackageList.java
@@ -0,0 +1,85 @@
+
+package com.facebook.react;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.res.Resources;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainPackageConfig;
+import com.facebook.react.shell.MainReactPackage;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+// @react-native-community/masked-view
+import org.reactnative.maskedview.RNCMaskedViewPackage;
+// react-native-biometrics
+import com.rnbiometrics.ReactNativeBiometricsPackage;
+// react-native-gesture-handler
+import com.swmansion.gesturehandler.RNGestureHandlerPackage;
+// react-native-local-auth
+import io.tradle.reactlocalauth.LocalAuthPackage;
+// react-native-safe-area-context
+import com.th3rdwave.safeareacontext.SafeAreaContextPackage;
+// react-native-screens
+import com.swmansion.rnscreens.RNScreensPackage;
+// react-native-sensitive-info
+import dev.mcodex.RNSensitiveInfo.RNSensitiveInfoPackage;
+// react-native-touch-id
+import com.rnfingerprint.FingerprintAuthPackage;
+
+public class PackageList {
+  private Application application;
+  private ReactNativeHost reactNativeHost;
+  private MainPackageConfig mConfig;
+
+  public PackageList(ReactNativeHost reactNativeHost) {
+    this(reactNativeHost, null);
+  }
+
+  public PackageList(Application application) {
+    this(application, null);
+  }
+
+  public PackageList(ReactNativeHost reactNativeHost, MainPackageConfig config) {
+    this.reactNativeHost = reactNativeHost;
+    mConfig = config;
+  }
+
+  public PackageList(Application application, MainPackageConfig config) {
+    this.reactNativeHost = null;
+    this.application = application;
+    mConfig = config;
+  }
+
+  private ReactNativeHost getReactNativeHost() {
+    return this.reactNativeHost;
+  }
+
+  private Resources getResources() {
+    return this.getApplication().getResources();
+  }
+
+  private Application getApplication() {
+    if (this.reactNativeHost == null) return this.application;
+    return this.reactNativeHost.getApplication();
+  }
+
+  private Context getApplicationContext() {
+    return this.getApplication().getApplicationContext();
+  }
+
+  public ArrayList<ReactPackage> getPackages() {
+    return new ArrayList<>(Arrays.<ReactPackage>asList(
+      new MainReactPackage(mConfig),
+      new RNCMaskedViewPackage(),
+      new ReactNativeBiometricsPackage(),
+      new RNGestureHandlerPackage(),
+      new LocalAuthPackage(),
+      new SafeAreaContextPackage(),
+      new RNScreensPackage(),
+      new RNSensitiveInfoPackage(),
+      new FingerprintAuthPackage()
+    ));
+  }
+}
