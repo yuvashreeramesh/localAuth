38e57ca6e3c126b0a9333c383de12a51723b72c5
yuvasreeramesh <yuvashree.doodleblue@gmail.com>
2023-08-02 12:19:01 +0530

    toggle integrated

--- App.js
+++ App.js
@@ -1,23 +1,71 @@
-import * as React from 'react';
-import { View, Text } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useEffect, useState } from 'react';
+import { View, Text, AppState, Alert } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import LocalAuth from 'react-native-local-auth';
 import HomeScreen from './screens/HomeScreen';
-const Stack = createNativeStackNavigator();
 
-function App() {
- 
- 
+const App = () => {
+  const [appLocked, setAppLocked] = useState(false);
+
+  useEffect(() => {
+    getAppStatus();
+  }, []);
+
+  useEffect(() => {
+    if (appLocked) {
+      handleAuthentication();
+    }
+  }, [appLocked]);
+
+  const handleAuthentication = () => {
+    LocalAuth.authenticate({
+      reason: 'This is a secure area, please authenticate yourself',
+      fallbackToPasscode: true,
+      suppressEnterPassword: true,
+    })
+      .then(success => {
+        console.log('Authenticated Successfully');
+      })
+      .catch(error => {
+        console.log('Authentication Failed', error.message);
+      });
+  };
+
+  const saveAppStatus = async (value) => {
+    try {
+      await AsyncStorage.setItem('@app_lock_enabled', value.toString());
+      setAppLocked(value); // Update the appLocked state with the new value
+    } catch (error) {
+      console.log('Error saving app lock status:', error);
+    }
+  };
+
+  const getAppStatus = async () => {
+    try {
+      const value = await AsyncStorage.getItem('@app_lock_enabled');
+      if (value !== null) {
+        setAppLocked(value === 'true');
+      }
+    } catch (error) {
+      console.error('Error loading app lock status:', error);
+    }
+  };
+
+  const handleToggle = (value) => {
+    console.log('toggle', value);
+    saveAppStatus(value);
+  };
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        {/* <Stack.Screen name="ProtectedScreen" component={ProtectedScreen} /> */}
-        <Stack.Screen name="Home" component={HomeScreen}  />
-
-
-      </Stack.Navigator>
-    </NavigationContainer>
+    
+   
+    <View style={{flex:1,alignItems:"center",justifyContent:"center"}}>
+      <Text style={{color:"white",fontSize:15}}>Swipe to enable app lock</Text>
+      <HomeScreen value={appLocked} onValueChange={handleToggle} />
+    </View>
+    
+   
   );
-}
+};
 
 export default App;
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,42 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, TouchableHighlight, Alert } from 'react-native';
-import LocalAuth from 'react-native-local-auth';
+import React, { useState,useEffect } from 'react';
+import { View, Text, Switch, StyleSheet } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const HomeScreen= () => {
-  const [authenticated, setAuthenticated] = useState(false);
-
-  const _pressHandler = () => {
-    LocalAuth.authenticate({
-      reason: 'please authenticate yourself',
-      fallbackToPasscode: true,
-      suppressEnterPassword: true,
-    })
-      .then(success => {
-        console.log('Authenticated Successfully');
-        setAuthenticated(true);
-      })
-      .catch(error => {
-        console.log('Authentication Failed', error.message);
-        setAuthenticated(false);
-      });
-  };
-
-  useEffect(() => {
-    _pressHandler();
-  }, []);
-
-  return (
-    <View>
-     
-      {authenticated ? (
-        <Text style={{ fontSize: 20, color: 'black' }}>Hi, you are authenticated!</Text>
-      ) : (
-        <Text style={{ fontSize: 20, color: 'black' }}>
-          To continue, authenticate yourself
-        </Text>
-      )}
-    </View>
-  );
-};
-
+const HomeScreen = ({value,onValueChange}) => {
+ 
+  return(
+      <Switch
+      value={value}
+      onValueChange={onValueChange}/>
+     )
+}
 export default HomeScreen;
